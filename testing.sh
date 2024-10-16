de01481a4b3f0f00419c3122a3160ae6164e7ac7
Andreeha <48863136+Andreeha@users.noreply.github.com>
2024-10-16 19:49:52 +0300

    Create testing.sh

---
+++ testing.sh
@@ -0,0 +1,8 @@
+echo "Test 1:"
+./easy_compress.sh ./test_folder00
+echo "Test 2:"
+./easy_compress.sh ./test_folder01 60
+echo "Test 3:"
+./easy_compress.sh ./test_folder02 50
+echo "Test 4:"
+./easy_compress.sh ./test_folder03 40
